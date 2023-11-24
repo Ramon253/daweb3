56ab6b3b57c79acce19061cb4c6d1b7242055825
Ramon253 <jgalazc253@g.educaand.es>
2023-11-24 18:25:16 +0100

    Feature concesionario.js terminada , falta testear

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT,() =>{
     console.log(`Server running on port ${PORT}`)
 })
-app.use('/concesionarios', require('./routers/concesionario'));
+app.use('/concesionarios', require('./router/concesionario'));
